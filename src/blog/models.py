ffd165f00cc462d592423ae2196ce621cbb7c886
mohsen_m <mohsen_m@local.com>
2025-10-22 16:26:39 +0330

    addd blog app and tested

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from pgvector.django import VectorField
 # Create your models here.
+ 
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    embedding = VectorField(dimensions=384, blank=True, null=True)
+    can_delete = models.BooleanField(default=False, help_text="Use in jupyter notebooks")
+    
